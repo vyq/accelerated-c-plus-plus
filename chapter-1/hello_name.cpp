159e9fb81c73c9fd705c73b0ad70bdd6a7320089
vyq <>
2018-05-02 06:09:47 +0800

    Say hello

---
+++ chapter-1/hello_name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  cout << "Name: ";
+
+  string name;
+  cin >> name;
+
+  cout << "Hello " << name << "!" << endl;
+
+  return 0;
+}
