887853df332cc1a60b57ac7a75d80cacc32605a7
vyq <>
2018-05-03 13:50:29 +0800

    Add dynamic greeting

---
+++ chapter-2/dynamic_greeting.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  cout << "Name: ";
+  string name;
+  cin >> name;
+
+  const string kGreeting {"Hello " + name + "!"};
+  const int pad {1};
+  const int rows {pad * 2 + 3};
+  const string::size_type columns {kGreeting.size() + pad * 2 + 2};
+
+  cout << endl;
+
+  // Invariant: Wrote r rows so far
+  for (int r = 0; r != rows; ++r) {
+    string::size_type c = 0;
+
+    // Invariant: Wrote c characters so far in current row
+    while (c != columns) {
+      if (r == pad + 1 && c == pad + 1) {
+        cout << kGreeting;
+        c += kGreeting.size();
+      } else {
+        if (r == 0 || r == rows - 1 || c == 0 || c == columns - 1)
+          cout << "*";
+        else
+          cout << " ";
+        ++c;
+      }
+    }
+
+    cout << endl;
+  }
+
+  return 0;
+}
