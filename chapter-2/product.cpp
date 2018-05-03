cb3f25e6993df813f4846a1cc90bfcef9a3b8573
vyq <>
2018-05-03 16:00:22 +0800

    Multiply integers

---
+++ chapter-2/product.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  const int maximum {10};
+  const int minimum {1};
+
+  for (int i = minimum; i < maximum; ++i) {
+    cout << i * i << endl;
+  }
+
+  return 0;
+}
