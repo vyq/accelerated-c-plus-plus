bd17e0d8f4131bc8afda23d6b2d76a8d355954a9
vyq <>
2018-05-03 15:57:52 +0800

    Countdown from 10 to -5

---
+++ chapter-2/countdown.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  const int maximum {10};
+  const int minimum {-6};
+
+  for (int i = maximum; i > minimum; --i) {
+    cout << i << endl;
+  }
+
+  return 0;
+}
