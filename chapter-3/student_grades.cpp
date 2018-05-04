359c345bf5fb42b77634f9e139f9bcf0cb581df2
vyq <>
2018-05-04 14:13:09 +0800

    Compute student grades

---
+++ chapter-3/student_grades.cpp
@@ -0,0 +1,81 @@
+#include <algorithm>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  map<string, double> student_grades;
+
+  // Invariant: student_grades contains all student grades read so far
+  while (true) {
+    cout << "Name: ";
+    string name;
+    cin >> name;
+    cout << "Hello " << name << "!" << endl;
+  
+    cout << "Midterm and final exam grades: ";
+    double midterm, final;
+    cin >> midterm >> final;
+  
+    cout << "Homework grades: ";
+    vector<double> homework;
+    double x;
+  
+    // Invariant: homework contains all grades read so far
+    while (cin >> x) {
+      homework.push_back(x);
+    }
+  
+    cin.clear();
+
+    typedef vector<double>::size_type vector_size;
+    vector_size size {homework.size()};
+  
+    if (size == 0) {
+      cout << "Please input homework grades.";
+  
+      return 1;
+    }
+  
+    sort(homework.begin(), homework.end());
+  
+    vector_size middle {size / 2};
+    double median {
+      size % 2 == 0 ?
+      (homework[middle] + homework[middle - 1]) / 2 :
+      homework[middle]
+    };
+  
+    double final_grade = 0.2 * midterm + 0.4 * final + 0.4 * median;
+
+    student_grades[name] = final_grade;
+
+    cout << "Input more student grades? (Yes / No) ";
+    string reply;
+    cin >> reply;
+
+    cout << endl;
+
+    if (reply == "No")
+      break;
+  }
+
+  // Invariant: Printed all grades read from student_grades so far
+  for (auto student_grade: student_grades) {
+    streamsize precision {cout.precision()};
+    cout <<
+      student_grade.first <<
+      ": " <<
+      setprecision(3) <<
+      student_grade.second <<
+      setprecision(precision) <<
+      endl;
+  }
+
+  return 0;
+}
