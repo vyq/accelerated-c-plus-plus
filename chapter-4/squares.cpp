99875e0c6ef9865206b101a099a73de45fa8b944
vyq <>
2018-05-06 16:33:13 +0800

    Refactor

--- chapter-4/squares.cpp
+++ chapter-4/squares.cpp
@@ -4,12 +4,21 @@
 using namespace std;
 
 int main() {
-  int upper_bound {1000};
-  string::size_type length {to_string(upper_bound).length()};
+  double upper_bound {1000};
+  string::size_type number_length {to_string(upper_bound).length()};
+  string::size_type square_length {
+    to_string(upper_bound * upper_bound).length()
+  };
 
   // Invariant: Computed i squares so far
-  for (int i = 0; i != upper_bound; ++i)
-    cout << setw(length) << i << " | " << i * i << endl;
+  for (double i = 0; i != upper_bound; ++i)
+    cout <<
+      setw(number_length) <<
+      i <<
+      ": " <<
+      setw(square_length) <<
+      i * i <<
+      endl;
 
   return 0;
 }
