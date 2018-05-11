0296bbad1b2914cc1be33199de5e24893b6dab00
vyq <>
2018-05-11 10:54:07 +0800

    Get longest length

---
+++ chapter-5/greeting.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string::size_type GetLongestLength(const vector<string>& v) {
+  string::size_type length = 0;
+
+  // Invariant: Processed i strings so far
+  for (vector<string>::size_type i = 0; i != v.size(); ++i)
+    length = max(length, v[i].size());
+
+  return length;
+}
+
+int main() {
+  cout << "Name: ";
+  string name;
+  cin >> name;
+
+  cout << "Space between greeting and left and right border: ";
+  int padLeftRight;
+  cin >> padLeftRight;
+
+  cout << "Space between greeting and top and bottom border: ";
+  int padTopBottom;
+  cin >> padTopBottom;
+
+  const string kGreeting {"Hello " + name + "!"};
+  const int rows {padTopBottom * 2 + 3};
+  const string::size_type columns {
+    kGreeting.size() +
+    padLeftRight * 2 +
+    2
+  };
+
+  cout << endl;
+
+  // Invariant: Wrote r rows so far
+  for (int r = 0; r != rows; ++r) {
+    string::size_type c = 0;
+
+    if (r == padTopBottom + 1) {
+      const string padding(padLeftRight, ' ');
+      cout << "*" << padding << kGreeting << padding << "*";
+    } else if (r == 0 || r == rows - 1) {
+      const string border(columns, '*');
+      cout << border;
+    } else {
+      const string padding(columns - 2, ' ');
+      cout << "*" << padding << "*";
+    }
+
+    cout << endl;
+  }
+
+  return 0;
+}
