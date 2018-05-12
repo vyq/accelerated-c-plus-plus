bd4cec35fd1ba74f2073c3c2c3b255fb2cf51791
vyq <>
2018-05-12 09:05:31 +0800

    Concatenate horizontally

--- chapter-5/greeting.cpp
+++ chapter-5/greeting.cpp
@@ -45,6 +45,34 @@ vector<string> ConcatenateVertically(
   return items;
 }
 
+vector<string> ConcatenateHorizontally(
+  const vector<string>& left,
+  const vector<string>& right
+) {
+  vector<string> items;
+
+  string::size_type space {GetLongestLength(left) + 1};
+
+  vector<string>::size_type i {0}, j {0};
+
+  // Invariant: Have not seen all rows in left and right
+  while (i != left.size() || j != right.size()) {
+    string s;
+
+    if (i != left.size())
+      s = left[i++];
+
+    s += string(space - s.size(), ' ');
+
+    if (j != right.size())
+      s += right[j++];
+
+    items.push_back(s);
+  }
+
+  return items;
+}
+
 int main() {
   cout << "Name: ";
   string name;
