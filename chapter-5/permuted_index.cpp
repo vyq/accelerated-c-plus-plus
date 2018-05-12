c30252eedfb41eb3d24f61690beeb427838db816
vyq <>
2018-05-12 15:06:05 +0800

    Format output

--- chapter-5/permuted_index.cpp
+++ chapter-5/permuted_index.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <iostream>
 #include <map>
 #include <string>
@@ -11,19 +12,21 @@ int main() {
   stringstream input("The quick brown fox\njumped over the fence");
   string s;
   map<string, string> items;
+  string::size_type length = 0;
 
   // Invariant: Stored all segments read so far
   while (getline(input, s, '\n')) {
     vector<string> v {Split(s)};
     vector<string>::size_type r = 0;
+    length = max(length, s.size());
 
     // Invariant: Made r rotations so far
     while (r != v.size()) {
       if (r == 0) {
         items[s] = " ";
       } else {
-        vector<string> key(v.begin(), v.begin() + r);
-        vector<string> value(v.begin() + r, v.end());
+        vector<string> key(v.begin() + r, v.end());
+        vector<string> value(v.begin(), v.begin() + r);
         string k, v;
 
         // Invariant: Stored all references read so far
@@ -43,7 +46,7 @@ int main() {
 
   // Invariant: Wrote all references read so far
   for (auto& item: items)
-    cout << item.first << " | " << item.second << endl;
+    cout << setw(length) << item.second << "|" << item.first << endl;
 
   return 0;
 }
