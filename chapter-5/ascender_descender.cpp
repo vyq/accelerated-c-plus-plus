06fe1c518714c332f53439c827e836657329687b
vyq <>
2018-05-16 14:21:40 +0800

    Refactor

--- chapter-5/ascender_descender.cpp
+++ chapter-5/ascender_descender.cpp
@@ -1,13 +1,37 @@
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int main() {
+bool HasAscender(string& w) {
   vector<char> ascenders {'b', 'd', 'f', 'h', 'k', 'l', 't'};
+
+  for (auto& c: w) {
+    if (find(ascenders.begin(), ascenders.end(), c) != ascenders.end())
+      return true;
+
+    return false;
+  }
+}
+
+bool HasDescender(string& w) {
   vector<char> descenders {'g', 'j', 'p', 'q', 'y'};
+
+  for (auto& c: w) {
+    if (
+      find(descenders.begin(), descenders.end(), c) !=
+      descenders.end()
+    )
+      return true;
+
+    return false;
+  }
+}
+
+int main() {
   bool has_ascender {false};
   bool has_descender {false};
 
@@ -15,21 +39,17 @@ int main() {
   string word;
   cin >> word;
 
-  for (auto& c: word) {
-    if (find(ascenders.begin(), ascenders.end(), c) != ascenders.end())
-      has_ascender = true;
-    if (
-      find(descenders.begin(), descenders.end(), c) !=
-      descenders.end()
-    )
-      has_descender = true;
-  }
+  has_ascender = HasAscender(word);
+  has_descender = HasDescender(word);
 
   if (has_ascender)
     cout << "Word has ascender" << endl;
 
   if (has_descender)
     cout << "Word has descender" << endl;
+
+  if (!has_ascender && !has_descender)
+    cout << "Word has no ascender and descender" << endl;
 
   return 0;
 }
