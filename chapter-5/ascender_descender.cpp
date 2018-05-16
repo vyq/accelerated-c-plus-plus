ceb93458363b6e27b91b8710949eb8eeabc61260
vyq <>
2018-05-16 14:34:16 +0800

    Fix ascender and descender

--- chapter-5/ascender_descender.cpp
+++ chapter-5/ascender_descender.cpp
@@ -7,17 +7,19 @@
 using namespace std;
 
 bool HasAscender(string& w) {
+  bool has_ascender {false};
   vector<char> ascenders {'b', 'd', 'f', 'h', 'k', 'l', 't'};
 
   for (auto& c: w) {
     if (find(ascenders.begin(), ascenders.end(), c) != ascenders.end())
-      return true;
-
-    return false;
+      has_ascender = true;
   }
+
+  return has_ascender;
 }
 
 bool HasDescender(string& w) {
+  bool has_descender {false};
   vector<char> descenders {'g', 'j', 'p', 'q', 'y'};
 
   for (auto& c: w) {
@@ -25,10 +27,10 @@ bool HasDescender(string& w) {
       find(descenders.begin(), descenders.end(), c) !=
       descenders.end()
     )
-      return true;
-
-    return false;
+      has_descender = true;
   }
+
+  return has_descender;
 }
 
 int main() {
@@ -50,6 +52,28 @@ int main() {
 
   if (!has_ascender && !has_descender)
     cout << "Word has no ascender and descender" << endl;
+
+  string::size_type size {0};
+  string longest_word;
+  string filename {"american_english_dictionary"};
+
+  ifstream file(filename);
+
+  // Invariant: Processed all words from file so far
+  while (getline(file, word)) {
+    has_ascender = HasAscender(word);
+    has_descender = HasDescender(word);
+
+    if (!has_ascender && !has_descender && word.size() > size) {
+      longest_word = word;
+      size = word.size();
+    }
+  }
+
+  cout <<
+    "Longest word with no ascenders or descenders: " <<
+    longest_word <<
+    endl;
 
   return 0;
 }
