0937e979198f13fdc5133249f87b4fe7c17a90d8
vyq <>
2018-05-16 13:40:43 +0800

    Find longest palindrome

--- chapter-5/palindrome.cpp
+++ chapter-5/palindrome.cpp
@@ -8,14 +8,24 @@ int main() {
   string filename {"american_english_dictionary"};
   ifstream file(filename);
   string word;
+  string longest_palindrome;
+  string::size_type size {0};
 
   // Invariant: Processed all words read from file so far
   while (getline(file, word)) {
     string reversed_word (word.rbegin(), word.rend());
 
-    if (word == reversed_word)
+    if (word == reversed_word) {
       cout << word << endl;
+
+      if (word.size() > size) {
+        longest_palindrome = word;
+        size = word.size();
+      }
+    }
   }
+
+  cout << endl << "Longest palindrome: " << longest_palindrome << endl;
 
   return 0;
 }
