ae184200b7ae56cdc8a1104e900ebb8b8ef73cd0
vyq <>
2018-06-15 09:37:24 +0800

    Refactor

--- chapter-6/student.cpp
+++ chapter-6/student.cpp
@@ -28,7 +28,6 @@ container_type GetFinalGreaterThanMidtermStudents(container_type s) {
   container_type::iterator iterator {
     stable_partition(s.begin(), s.end(), IsFinalGreaterThanMidterm)
   };
-  container_type greater_than(iterator, s.end());
   s.erase(iterator, s.end());
 
   return s;
@@ -40,7 +39,6 @@ container_type GetFinalNotGreaterThanMidtermStudents(
   container_type::iterator iterator {
     stable_partition(s.begin(), s.end(), IsFinalNotGreaterThanMidterm)
   };
-  container_type not_greater_than(iterator, s.end());
   s.erase(iterator, s.end());
 
   return s;
