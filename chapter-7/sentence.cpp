1b013493db1bd1caae648c737563dee44c5bcbf7
vyq <>
2018-06-23 11:29:03 +0800

    Expand grammar

--- chapter-7/sentence.cpp
+++ chapter-7/sentence.cpp
@@ -10,6 +10,10 @@ typedef vector<string> Rule;
 typedef vector<Rule> Rules;
 typedef map<string, Rules> Grammar;
 
+int Randomize(int maximum) {
+  return maximum;
+}
+
 Grammar ReadGrammar(istream& in) {
   Grammar result;
   string line;
@@ -21,6 +25,44 @@ Grammar ReadGrammar(istream& in) {
     if (!items.empty())
       result[items[0]].push_back(Rule(items.begin() + 1, items.end()));
   }
+
+  return result;
+}
+
+bool IsTag(const string& s) {
+  return s.size() > 1 && s[0] == '<' && s[s.size() - 1] == '>';
+}
+
+void Expand(
+  const Grammar& grammar,
+  const string& word,
+  vector<string>& result
+) {
+  if (!IsTag(word)) {
+    result.push_back(word);
+  } else {
+    Grammar::const_iterator iterator {grammar.find(word)};
+
+    if (iterator == grammar.end())
+      throw domain_error("Invalid rule");
+
+    const Rules& rules {iterator->second};
+    const Rule& rule {rules[Randomize(rules.size())]};
+
+    for (
+      Rule::const_iterator iterator = rule.begin();
+      iterator != rule.end();
+      ++iterator
+    ) {
+      Expand(grammar, *iterator, result);
+    }
+  }
+}
+
+vector<string> MakeSentence(const Grammar& g) {
+  vector<string> result;
+
+  Expand(g, "<sentence>", result);
 
   return result;
 }
