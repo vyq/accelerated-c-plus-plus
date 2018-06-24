6add5f9dce9621da5a0fa32eeea9f8313b23524a
vyq <>
2018-06-24 10:26:29 +0800

    Group words by count

--- chapter-7/count.cpp
+++ chapter-7/count.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
   string s;
   map<string, int> counters;
+  map<int, vector<string>> groups;
 
   // Invariant: Counted all inputs read from cin so far
   while (cin >> s)
     ++counters[s];
+
+  cout << endl << "Word\tCount" << endl;
 
   // Invariant: Wrote all inputs and their associated counts so far
   for (
@@ -19,6 +23,24 @@ int main() {
     ++iterator
   ) {
     cout << iterator->first << "\t" << iterator->second << endl;
+
+    groups[iterator->second].push_back(iterator->first);
+  }
+
+  cout << endl << "Count\tWord(s)" << endl;
+
+  // Invariant: Wrote all inputs grouped by counts so far
+  for (
+    map<int, vector<string>>::const_iterator iterator = groups.begin();
+    iterator != groups.end();
+    ++iterator
+  ) {
+    cout << iterator->first << "\t";
+
+    for (auto& element : iterator->second)
+      cout << element << " ";
+
+    cout << endl;
   }
 
   return 0;
