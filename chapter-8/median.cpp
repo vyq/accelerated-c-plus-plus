5d9a7a2eaf2c4afe0e13725ddcee4cc8eead740c
vyq <>
2018-08-05 06:53:17 +0800

    Compute median

---
+++ chapter-8/median.cpp
@@ -0,0 +1,24 @@
+#include <stdexcept>
+#include <vector>
+
+using namespace std;
+
+template<class T>
+T ComputeMedian(vector<T> v) {
+  typedef typename vector<T>::size_type size_type;
+
+  size_type size = v.size();
+
+  if (size == 0)
+    throw domain_error("Empty vector");
+
+  sort(v.begin(), v.end());
+
+  size_type mid = size / 2;
+
+  return size % 2 == 0 ? (v[mid] + v[mid - 1]) / 2 : v[mid];
+}
+
+int main() {
+  return 0;
+}
